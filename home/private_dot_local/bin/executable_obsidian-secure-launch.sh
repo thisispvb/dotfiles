1c6cc52229736507e7480bbbdc3bb689ef55ff7b
Philip von Bargen <p@bargen.co>
2025-02-13 14:28:40 -0500

    Create encrypted disk image for Obsidian vault if it doesn't exist yet

--- home/private_dot_local/bin/executable_obsidian-secure-launch.sh
+++ home/private_dot_local/bin/executable_obsidian-secure-launch.sh
@@ -3,10 +3,24 @@
 set -eufo pipefail
 
 encryption_key=$(op read "op://Private/macOS Sensitive Vault Image Encryption Key/password")
-image_path="$HOME/vaults/Obsidian Vaults.dmg"
+vault_name="Obsidian Vaults"
+image_path="$HOME/vaults/$vault_name.dmg"
+
+# Create encrypted disk image if it doesn't exist yet
+if [[ ! -f "$image_path" ]]; then
+  echo "Warning: Disk Image does not exist at \"$image_path\", creating a new one..."
+  printf '%s\0' "$encryption_key" | hdiutil create "$image_path" \
+    -size 5g \
+    -fs APFS \
+    -volname "$vault_name" \
+    -type UDIF \
+    -encryption AES-256 \
+    -stdinpass
+fi
 
 # Mount the encrypted disk image
-printf '%s\0' "$encryption_key" | hdiutil mount "$image_path" -stdinpass
+printf '%s\0' "$encryption_key" | hdiutil mount "$image_path" -quiet -stdinpass
 
 # Launch Obsidian
+echo "Successfully mounted Disk Image from \"$image_path\""
 open /Applications/Obsidian.app
